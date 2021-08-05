1c6eae75e69d0ba0a0b7b7b1bde5aeb3fe73faf7
ujin2021 <ujin2021@naver.com>
2021-08-05 15:30:38 +0900

    [String, Date] 중성화 여부 파악하기

---
+++ programmers-sql/7.sql
@@ -0,0 +1,8 @@
+-- [String, Date] 중성화 여부 파악하기
+SELECT ANIMAL_ID, NAME, 
+CASE WHEN SEX_UPON_INTAKE LIKE 'Intact%' THEN 'X'
+     -- WHEN SEX_UPON_INTAKE NOT LIKE 'Intact%' THEN 'O'
+     ELSE 'O'
+     END AS 중성화
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
